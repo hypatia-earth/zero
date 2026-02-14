a6ddf9c5f76d9a723db660d9d9cf7d7cce41353b
operator <operator@hypatia.earth>
2026-02-14 16:18:40 +0100

    fix: URL has precedence over IDB for custom layer enabled state

--- src/services/state-service.ts
+++ src/services/state-service.ts
@@ -137,8 +137,8 @@ export class StateService {
       this.viewState.value = { ...this.viewState.value, time: newTime };
     }
 
-    // Write sanitized state to URL
-    this.syncToUrl();
+    // Schedule URL sync (will only run after enableUrlSync is called)
+    this.scheduleUrlSync();
   }
 
   /** Enable URL sync after bootstrap complete */
